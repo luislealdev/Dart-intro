3430f6d21e4a226036f7b0547c7e550521c0a758
Luis Leal <luisrrleal@gmail.com>
2023-06-28 22:14:26 -0600

    First commit

---
+++ hello-world.dart
@@ -0,0 +1,4 @@
+void main() {
+  var name = 'Luis';
+  print('Hello $name!');
+}
---
+++ variables.dart
@@ -0,0 +1,15 @@
+void main() {
+  final String pokemon = "pikachu";
+  final int power = 100;
+  final bool isAlive = true;
+  final List<String> abilities = ["Fire"];
+  final List<String> sprites = ["front.png", "back.png"];
+
+  print("""
+  $pokemon
+  $power
+  $isAlive
+  $abilities
+  $sprites
+""");
+}
